07af9fbbe1798a6b230c1635e0ecd9995b1028be
Pedro Santos <github@mariodelrio.com>
2016-06-10 22:03:30 +0100

    Reorder types and fitness match expression

--- TreasureHunt/Fitness.fs
+++ TreasureHunt/Fitness.fs
@@ -1,10 +1,10 @@
 ﻿module Fitness    
-    type Content = Empty | Treasure | Wall
-    type Action = Pick | StayPut | North | South | East | West
     type Position = Position of int * int
-    type ActionOutcome = {fitness:int; nextPosition:Position}
+    type Content = Empty | Treasure | Wall
     type Site = Position * Content 
     type Situation = {current:Site; north:Site; south:Site; west:Site; east:Site}
+    type ActionOutcome = {fitness:int; nextPosition:Position}
+    type Action = Pick | StayPut | North | South | East | West
 
     let private siteContent site = snd site
 
@@ -25,11 +25,11 @@
 
     let fitness content action = 
         match content, action with
+        | Empty, Pick -> -1
+        | Treasure , Pick -> 10
         | Wall, _ -> -5
         | _ , StayPut -> 0
         | _, North | _, South | _, East | _, West -> 0
-        | Empty, _ -> -1
-        | Treasure , Pick -> 10
 
     let outcomeAfterAction action situation =
         let contentAfterAction = situation |> siteAfterAction action |> siteContent
