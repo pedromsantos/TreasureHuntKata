94f9be5ce2e89cadb1c14dba1a21586ce0d090f1
Alessandro Di Gioia <alex.digioia@gmail.com>
2016-06-11 23:34:23 +0100

    renamed treasureRandomTop to maxTreasures, redefined maxTreasures as size squared, redefined treasureThresholdChance in terms of maxTreasures, renamed contentForPosition as contentAt

--- TreasureHunt/Grid.fs
+++ TreasureHunt/Grid.fs
@@ -4,12 +4,15 @@ module Grid
     type Grid = Site seq
 
     let private size = 10
-    let private treasureRandomTop = size * 10
-    let private treasureThresholdChance = 60
+    let private maxTreasures = size * size
+    let private treasureThresholdChance = (int)((float)maxTreasures * 0.5)
     let private rand = System.Random()
 
-    let private TreasureOrEmpty = 
-        let number = rand.Next(1, treasureRandomTop)
+    let private nextRand min max =
+        rand.Next(min, max)
+
+    let private treasureOrEmpty =
+        let number = nextRand 1 maxTreasures
         match number with
         | n when n < treasureThresholdChance -> Treasure
         | _ -> Empty
@@ -17,18 +20,18 @@ module Grid
     let private isWall line = 
         line = 0 || line = size - 1
 
-    let private contentForPosition row column = 
+    let private contentAt row column = 
         let content =
-            if isWall row || isWall column 
+            if row |> isWall || column |> isWall 
             then Wall
-            else TreasureOrEmpty
+            else treasureOrEmpty
         Position(row, column), content
 
     let create = 
         seq { 
             for row in 0 .. size - 1 do
                 for column in 0 .. size - 1 do
-                    yield contentForPosition row column
+                    yield contentAt row column
         }
 
     let findSiteBy position grid =
